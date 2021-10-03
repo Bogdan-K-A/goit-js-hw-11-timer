4a95a1622b2dab0e4584ef4f8afa448334ef4e21
Bogdan-K-A <kozhoma_b@ukr.net>
2021-10-03 20:18:40 +0300

    timer

---
+++ js/add.js
@@ -0,0 +1,81 @@
+const refs = {
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]'),
+  day: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+  clockface: document.querySelector('#timer-1'),
+};
+// ==============================
+const timer = {
+  intervalId: null,
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const startTime = new Date('Dec 31 2021 00:00:00');
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = new Date();
+      const deltaTime = startTime - currentTime;
+      const time = getTimeComponents(deltaTime);
+
+      updateClockface(time);
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    zeroTime();
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  timer.stop();
+});
+
+function updateClockface({ days, hours, mins, secs }) {
+  refs.day.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
+
+function zeroTime() {
+  refs.day.textContent = '00';
+  refs.hours.textContent = '00';
+  refs.mins.textContent = '00';
+  refs.secs.textContent = '00';
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
+
+// ===================================
+// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jan 31 2021 00:00:00'),
+// })
